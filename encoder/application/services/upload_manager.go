799b7ec466a90e63234d0f081be1133219ccc7f6
Renato Salvador <renato.salvador@bolttech.io>
2022-05-12 14:46:19 +0100

    upload object

---
+++ encoder/application/services/upload_manager.go
@@ -0,0 +1,48 @@
+package services
+
+import (
+	"context"
+	"io"
+	"os"
+	"strings"
+
+	"cloud.google.com/go/storage"
+)
+
+type VideoUpload struct {
+	VideoPath    string
+	OutputBucket string
+	Paths        []string
+	Errors       []string
+}
+
+func NewVideoUpload() *VideoUpload {
+	return &VideoUpload{}
+}
+
+func (vu *VideoUpload) UploadObject(objectPath string, client *storage.Client, ctx context.Context) error {
+	path := strings.Split(objectPath, os.Getenv("LOCAL_STORAGE_PATH")+"/")
+
+	f, err := os.Open(objectPath)
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+
+	wc := client.Bucket(vu.OutputBucket).Object(path[1]).NewWriter(ctx)
+	wc.ACL = []storage.ACLRule{{
+		Entity: storage.AllUsers,
+		Role:   storage.RoleReader,
+	}}
+
+	if _, err := io.Copy(wc, f); err != nil {
+		return err
+	}
+
+	if err := wc.Close(); err != nil {
+		return err
+	}
+
+	return nil
+}
